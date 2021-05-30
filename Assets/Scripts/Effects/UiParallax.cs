6345bbf21db498ad0be3552c928e6b32cd31e3d4
Rei <clxgamer12@gmail.com>
2021-05-30 05:27:52 -0400

    Menu (WIP) and Animated Intro
    Currently using a placeholder music by "audios by cowbelly". The music is safe to use on YouTube and elsewhere (copyright and royalty free).

---
+++ Assets/Scripts/Effects/UiParallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+ 
+public class UiParallax : MonoBehaviour {
+ 
+    private Vector3 _pz;
+    private Vector3 _startPos;
+    private Camera _mainCamera;
+     
+    public float modifier;
+     
+    void Start ()
+    {
+        _startPos = transform.position;
+        _mainCamera = Camera.main;
+    }
+     
+    void Update ()
+    {
+        var pz = _mainCamera.ScreenToViewportPoint(Input.mousePosition);
+        pz.z = 0;
+        gameObject.transform.position = pz;
+        transform.position = new Vector3(_startPos.x + (pz.x * (modifier/10)), _startPos.y + (pz.y * (modifier/10)), 0);
+    }
+     
+}
---
+++ Assets/Scripts/Menu/Intro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public float bpm;
+    public Animator introAnimator;
+
+    private float _currentBeatTimer;
+    private static readonly int Beat = Animator.StringToHash("Beat");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentBeatTimer = bpm / 60 * 2; //Every second beat
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentBeatTimer -= Time.deltaTime;
+        if (_currentBeatTimer <= 0)
+        {
+            introAnimator.SetTrigger(Beat);
+            _currentBeatTimer = bpm / 60 * 2;
+        }
+    }
+}

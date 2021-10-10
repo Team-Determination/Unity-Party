8aee7e08525fd9730d7f229eb1ce1bcd11dccc2d
Rei <clxgamer12@gmail.com>
2021-10-10 01:14:07 -0400

    Removed Unity-default user reporting system
    Flat out refused to work for some systems.

---
+++ Assets/Scripts/InGameReportInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameReportInitializer : MonoBehaviour
+{
+    public CrittrSDK crittr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crittr.ConnectionURI = "";
+    }
+}

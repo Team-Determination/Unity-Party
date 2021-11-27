5c15170e30ad04bac1e617fd0182a265cdddb41e
Rei <clxgamer12@gmail.com>
2021-11-26 23:21:10 -0500

    Game now has working ratings again
    An issue came up during the last implementation of lite mode (which was the previous commit)

--- Assets/Scripts/RatingObject.cs
+++ Assets/Scripts/RatingObject.cs
@@ -7,22 +7,27 @@ public class RatingObject : MonoBehaviour
 {
     public SpriteRenderer sprite;
     public float liteTimer;
+    public bool isLiteSprite;
     
     // Start is called before the first frame update
     void Start()
     {
-        if(!Song.instance.liteMode)
+        if(!Song.instance.liteMode & !isLiteSprite)
         {
             LeanTween.moveY(gameObject, transform.position.y - 0.2f, .75f).setOnComplete(() =>
             {
                 LeanTween.alpha(gameObject, 0, .45f).setDelay(1f).setOnComplete(() => { Destroy(gameObject); });
             }).setEase(LeanTweenType.easeOutBounce);
+        } else if (isLiteSprite)
+        {
+            sprite.enabled = false;
         }
     }
 
 
     private void Update()
     {
+        if (!Song.instance.liteMode) return;
         sprite.enabled = !(liteTimer <= 0);
         liteTimer -= Time.deltaTime;
     }
